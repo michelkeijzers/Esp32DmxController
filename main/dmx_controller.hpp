53e7435341f2d55d05e2cc544e98cef2b76ff1a5
Michel Keijzers <michelkeijzers@hotmail.com>
2026-02-13 03:35:45 +0100

    Stage, commit, and push all changes to external repository.

--- main/dmx_controller.hpp
+++ main/dmx_controller.hpp
@@ -1,26 +1,25 @@
 #pragma once
-#include <stdio.h>
+#include "artnet_sender.hpp"
+#include "dmx_preset_changer.hpp"
+#include "dmx_presets.hpp"
 #include "driver/gpio.h"
+#include "foot_switch.hpp"
+#include "nvs_storage.hpp"
+#include "osc_sender.hpp"
+#include "rtos_task.hpp"
+#include "seven_segment_display.hpp"
+#include "web_server.hpp"
+#include <esp_https_ota.h>
+#include <esp_log.h>
+#include <esp_ota_ops.h>
+#include <esp_wifi.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <freertos/timers.h>
-#include "foot_switch.hpp"
-#include "dmx_preset_changer.hpp"
-#include <esp_https_ota.h>
-#include <esp_ota_ops.h>
-#include <esp_log.h>
-#include <esp_wifi.h>
-#include "osc_sender.hpp"
-#include "dmx_presets.hpp"
-#include "seven_segment_display.hpp"
-#include "artnet_sender.hpp"
-#include "web_server.hpp"
-#include "rtos_task.hpp"
-#include "nvs_storage.hpp"
+#include <stdio.h>
 
-class DmxController : public RtosTask
-{
-public:
+class DmxController : public RtosTask {
+  public:
     DmxController();
     ~DmxController();
     esp_err_t init();
@@ -30,11 +29,10 @@ public:
     esp_err_t performOtaUpdate(const char *url);
     void printFirmwareInfo();
 
-private:
+  private:
     static constexpr gpio_num_t FOOT_SWITCH_PIN = GPIO_NUM_4;
     static constexpr gpio_num_t DISPLAY_PINS[8] = {
-        GPIO_NUM_1, GPIO_NUM_2, GPIO_NUM_3, GPIO_NUM_5,
-        GPIO_NUM_6, GPIO_NUM_7, GPIO_NUM_8, GPIO_NUM_9};
+        GPIO_NUM_1, GPIO_NUM_2, GPIO_NUM_3, GPIO_NUM_5, GPIO_NUM_6, GPIO_NUM_7, GPIO_NUM_8, GPIO_NUM_9};
     static constexpr const char *OTA_URL = "https://example.com/firmware.bin";
     static constexpr const char *OTA_CERT_PEM = nullptr;
     static constexpr const char *OSC_DEST_IP = "192.168.1.100";
